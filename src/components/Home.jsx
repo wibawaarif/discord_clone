93aa50ef80f47e9e80487192484d21f31723b6dd
wibawaarif <arifwb.work@gmail.com>
2023-04-13 22:48:45 +0700

    improve landing page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,21 +4,22 @@ import BgRightLanding from "../../assets/right_bg_landing_header.svg";
 import BgLeftLanding from "../../assets/left_bg_landing_header.svg";
 import BgCenterLanding from "../../assets/center_bg_landing_header.svg";
 import DiscordMainLogo from "../../assets/discord_main_logo.svg";
+import DiscordMain2Logo from "../../assets/invite_only_landing.svg";
 import Download from "../../assets/download.svg"
 
 const Navbar = ["Download", "Nitro", "Safety", "Support", "Blog", "Careers"];
 
 function Home() {
   return (
-    <div className="container h-max">
-      <div className="static bg-[#404EED]">
+    <div className="container">
+      <div style={{height: '625px'}} className="static bg-[#404EED] z-0">
         {/* Navbar */}
         <div className="px-56 py-5 flex items-center justify-between">
           <div>
             <img className="cursor-pointer" src={DiscordMainLogo} />
           </div>
 
-          <div className="w-2/4 flex justify-between">
+          <div className="w-2/4 flex justify-between z-0">
             {Navbar.map((x, index) => (
               <Link className="text-white font-semibold cursor-pointer hover:underline" key={`${x}-${index}`}>
                 {x}
@@ -31,6 +32,8 @@ function Home() {
           </button>
         </div>
 
+
+        {/* Hero 1 */}
         <div className="flex flex-col items-center text-center mt-[7.5rem]">
           <span style={{fontFamily: 'Seymour One'}} className="text-5xl text-white">
             IMAGINE A PLACE...
@@ -45,7 +48,7 @@ function Home() {
             </span>
           </div>
 
-          <div className="mt-11 flex items-center">
+          <div className="mt-11 flex items-center z-40">
             <button className="bg-white transition duration-[400ms] hover:text-[#5865f2] hover:shadow-xl rounded-full px-8 py-4 mr-4">
                 <div className="flex items-center">
                 <img className="w-6 h-6 mr-2" src={Download} />
@@ -57,19 +60,42 @@ function Home() {
             </button>
           </div>
         </div>
-
-
           <img
-            style={{ width: "530px", height: "400px" }}
-            className="absolute"
+            style={{ width: "700px" }}
+            className="absolute top-[16.7rem] left-[-17rem] z-20"
             src={BgLeftLanding}
           />
 
-          {/* <img src={BgCenterLanding} /> */}
+          
+          <img
+            style={{ width: "650px" }}
+            className="absolute top-[15.25rem] right-[-14.5rem] z-20"
+            src={BgRightLanding}
+          />
 
-          {/* <img src={BgRightLanding} /> */}
+          <img
+            style={{ width: "1500px", height: '500px' }}
+            className="absolute top-[11.95rem] z-10"
+            src={BgCenterLanding}
+          />
+      </div>
+
+      {/* Hero 2 */}
+      <div style={{height: '625px', width: '1100px'}} className="flex justify-center mx-auto items-center">
+
+              <img width={678} src={DiscordMain2Logo} />
+
+              <div className="flex flex-col ml-16"> 
+                <span style={{fontFamily: 'Lato'}} className="text-5xl text-[#23272a]">Create an invite-<br/>only place where you belong</span>
+
+                <span className="text-lg text-[#23272a]">Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat.</span>
+
+
+              </div>
+
 
       </div>
+
     </div>
   );
 }
