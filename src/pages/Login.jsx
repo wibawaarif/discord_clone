1ee6f749d383140ba7e845a44cb6e3a3fa7a71ed
wibawaarif <arifwb.work@gmail.com>
2023-04-14 22:36:08 +0700

    improve register page and add login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,32 @@
-
-function Login() {
+import { Link } from "react-router-dom";
+const Login = () => {
 
     return (
-        <div>Login</div>
+        <div className="container h-screen bg-cover w-screen bg-[url('../../assets/login_bg.svg')] flex justify-center items-center">
+
+            <div className="bg-[#313338] h-[25rem] w-[48rem] rounded-md flex justify-center items-center">
+                <div className="w-[26rem] h-[21rem]">
+                    <p>Welcome back!</p>
+                    <p>We're so excited to see you again!</p>
+                    <div className="flex flex-col justify-left mt-6">
+                        <p className="text-[#dcddde] text-xs font-bold">EMAIL</p>
+                        <input className="bg-[#1E1F22] h-[2.5rem] w-[26rem] text-[#dcddde] p-2 outline-none mt-2" />
+                    </div>
+                    <div className="flex flex-col justify-left mt-6">
+                        <p className="text-[#dcddde] text-xs font-bold">EMAIL</p>
+                        <input className="bg-[#1E1F22] h-[2.5rem] w-[26rem] text-[#dcddde] p-2 outline-none mt-2" />
+                    </div>
+                    <Link to="/login" className="text-[#049CEA] text-sm hover:underline hover:cursor-pointer">Already have an account?</Link>
+                    <button className="transition duration-[200ms] h-[2.5rem] w-[26rem] py-6 bg-[#5865F2] hover:bg-[#424ec2] flex items-center justify-center text-[#dcddde] font-bold rounded-sm">Log In</button>
+                    <p className="text-[0.7rem] text-[#b9bbbe] text-xs hover:underline hover:cursor-pointer mt-5">By registering, you agree to Discord's <span className="text-[#049CEA] hover:underline">Terms of Service</span> and <span className="text-[#049CEA] hover:underline">Privacy Policy</span></p>
+                </div>
+
+                <div className="bg-white w-72 flex flex-col items-center">
+                    <p>test</p>
+                </div>
+            </div>
+
+        </div>
     )
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,29 +1,31 @@
-import BgRegister from "../../assets/login_bg.svg";
-function Register() {
+import { Link } from "react-router-dom";
+const Register = () => {
 
     return (
-        <div className="container h-screen object-fill w-screen bg-[url('../../assets/login_bg.svg')] flex justify-center items-center">
+        <div className="container h-screen bg-cover w-screen bg-[url('../../assets/login_bg.svg')] flex justify-center items-center">
 
             <div className="bg-[#313338] h-[37rem] w-[30rem] rounded-md flex justify-center items-center">
-                <div className="h-[30rem] w-[28] flex flex-col justify-between items-center">
+                <div className="h-[30rem] w-[28] flex flex-col items-center">
                 <p className="text-2xl font-bold text-[#dcddde]">Create an account</p>
 
-                <div className="flex flex-col justify-left">
+                <div className="flex flex-col justify-left mt-6">
                     <p className="text-[#dcddde] text-xs font-bold">EMAIL</p>
                     <input className="bg-[#1E1F22] h-[2.5rem] w-[26rem] text-[#dcddde] p-2 outline-none mt-2" />
                 </div>
-                <div className="flex flex-col justify-left">
+                <div className="flex flex-col justify-left mt-6">
                     <p className="text-[#dcddde] text-xs font-bold">USERNAME</p>
                     <input className="bg-[#1E1F22] h-[2.5rem] w-[26rem] text-[#dcddde] p-2 outline-none mt-2" />
                 </div>
-                <div className="flex flex-col justify-left">
+                <div className="flex flex-col justify-left mt-6 mb-10">
                     <p className="text-[#dcddde] text-xs font-bold">PASSWORD</p>
                     <input type="password" className="bg-[#1E1F22] h-[2.5rem] w-[26rem] text-[#dcddde] p-2 outline-none mt-2" />
                 </div>
+
                 <button className="transition duration-[200ms] h-[2.5rem] w-[26rem] py-6 bg-[#5865F2] hover:bg-[#424ec2] flex items-center justify-center text-[#dcddde] font-bold rounded-sm">Continue</button>
 
-                <div className="text-left w-[26rem]">
-                <p className="text-[#049CEA] text-sm hover:underline hover:cursor-pointer">Already have an account?</p>
+                <div className="text-left w-[26rem] mt-2">
+                <Link to="/login" className="text-[#049CEA] text-sm hover:underline hover:cursor-pointer">Already have an account?</Link>
+                <p className="text-[0.7rem] text-[#b9bbbe] text-xs hover:underline hover:cursor-pointer mt-5">By registering, you agree to Discord's <span className="text-[#049CEA] hover:underline">Terms of Service</span> and <span className="text-[#049CEA] hover:underline">Privacy Policy</span></p>
                 </div>
 
                 </div>
