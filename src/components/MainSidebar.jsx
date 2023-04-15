f28afb81888247ba6c3cf54a6372c54259e1c8b0
wibawaarif <arifwb.work@gmail.com>
2023-04-15 15:34:01 +0700

    Improve dashboard (svg color. main sidebar)

--- src/components/MainSidebar.jsx
+++ src/components/MainSidebar.jsx
@@ -1,6 +1,7 @@
 import FriendLogo from "../../assets/friends.svg";
 import StageDiscoveryLogo from "../../assets/stage_discovery.svg";
 import NitroLogo from "../../assets/nitro_icon.svg";
+import PlusLogo from "../../assets/plus_icon.svg";
 
 const MainSidebar = () => {
   return (
@@ -13,9 +14,24 @@ const MainSidebar = () => {
 
         <div className="w-[14.5rem] h-[52rem]">
             
-        <div className="flex">
-            <img src={FriendLogo} />
-            <p>Friends</p>
+        <div className="flex rounded-md px-2 py-2 items-center mt-2 hover:cursor-pointer hover:bg-[#393C43]">
+            <img className="mr-5 fill-blue-500" width={28} height={28} src={FriendLogo} />
+            <p className="text-[#8e9297] font-medium">Friends</p>
+        </div>
+
+        <div className="flex rounded-md px-2 py-2 items-center mt-2 hover:cursor-pointer hover:bg-[#393C43]">
+            <img className="mr-5" width={28} height={28} src={StageDiscoveryLogo} />
+            <p className="text-white font-medium">Stage Discovery</p>
+        </div>
+
+        <div className="flex rounded-md px-2 py-2 items-center mt-2 hover:cursor-pointer hover:bg-[#393C43]">
+            <img className="mr-5" width={28} height={28} src={NitroLogo} />
+            <p className="text-white font-medium">Nitro</p>
+        </div>
+
+        <div className="flex justify-between mt-6 px-2">
+            <p className="font-semibold text-[11px] text-white hover:cursor-default">DIRECT MESSAGES</p>
+            <img className="hover:cursor-pointer" width={18} height={18} src={PlusLogo} />
         </div>
 
 
